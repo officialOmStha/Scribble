b9e2fbc45be1613cb4f6a7b0286b02431e4567bc
Om Official <omstha71@gmail.com>
2025-08-09 12:35:44 +0545

    initial setup of react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-3xl text-blue-500">Hello World</div>
+    </>
+  )
+}
+
+export default App
